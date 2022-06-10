16701bc19af54a88b4f79cb99c13f26b6f93f55a
Elias Mohammadi <elias.mohammady91@gmail.com>
2022-06-10 19:25:01 +0430

    refactor measurement models: use basicUnit as composition

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -2,39 +2,36 @@ const {describe, it} = require('mocha')
 const expect = require('chai').expect
 
 const {CoefficientUnit, BasicUnit, FormulatedUnit, Dimension} = require('../src/model/measurement.model')
-describe('MeasurementUnit', () => {
+describe.only('MeasurementUnit', () => {
 
 
     it('should convert CoefficientUnit to BasicUnit', (done) => {
         const dimension = new Dimension('length')
         const basicUnit = new BasicUnit('Meter', 'm', dimension)
-        const coUnit = new CoefficientUnit('MilliMeter', 'mm', dimension, 0.001)
-        const actualBasicUnit = coUnit.convertToBase(1,basicUnit)
+        const coUnit = new CoefficientUnit('MilliMeter', 'mm', basicUnit, 0.001)
+        const actualBasicUnit = coUnit.convertToBase(1)
         expect(actualBasicUnit.value).to.be.equal(0.001)
         expect(actualBasicUnit.name).to.be.equal('Meter')
         expect(actualBasicUnit.symbol).to.be.equal('m')
         expect(actualBasicUnit.dimension.name).to.be.equal('length')
         done()
     })
-    it("should convert BasicUnit from CoefficientUnit",(done)=>{
+    it("should convert BasicUnit from CoefficientUnit", (done) => {
         const dimension = new Dimension('length')
         const basicUnit = new BasicUnit('Meter', 'm', dimension)
         basicUnit.setValue(1)
-        const coUnit = new CoefficientUnit('MilliMeter', 'mm', dimension, 0.001)
-        const actualCoUnit = coUnit.convertFromBase(basicUnit)
-        expect(actualCoUnit.value).to.be.equal(1000)
-        expect(actualCoUnit.name).to.be.equal('MilliMeter')
-        expect(actualCoUnit.symbol).to.be.equal('mm')
-        expect(actualCoUnit.dimension.name).to.be.equal('length')
+        const coUnit = new CoefficientUnit('MilliMeter', 'mm', basicUnit, 0.001)
+        coUnit.convertFromBase()
+        expect(coUnit.value).to.be.equal(1000)
         done()
     })
 
-    it('should convert FormulatedUnit to BasicUnit',(done)=>{
+    it('should convert FormulatedUnit to BasicUnit', (done) => {
         const dimension = new Dimension('temperature')
-        const basicUnit = new BasicUnit('celsius','c',dimension)
-        const formulatedUnit = new FormulatedUnit('fahrenheit','F',dimension)
+        const basicUnit = new BasicUnit('celsius', 'c', dimension)
+        const formulatedUnit = new FormulatedUnit('fahrenheit', 'F', basicUnit)
         formulatedUnit.setFormulaToBase('((a-32) * 5)/9')
-        const actualBasicUnit = formulatedUnit.convertToBase(41,basicUnit)
+        const actualBasicUnit = formulatedUnit.convertToBase(41, basicUnit)
         expect(actualBasicUnit.name).to.be.equal('celsius')
         expect(actualBasicUnit.value).to.be.equal(5)
         expect(actualBasicUnit.symbol).to.be.equal('c')
@@ -42,18 +39,14 @@ describe('MeasurementUnit', () => {
         done()
     })
 
-    it('should convert BasicUnit to FormulatedUnit',(done)=>{
+    it('should convert BasicUnit to FormulatedUnit', (done) => {
         const dimension = new Dimension('temperature')
-        const basicUnit = new BasicUnit('celsius','c',dimension)
+        const basicUnit = new BasicUnit('celsius', 'c', dimension)
         basicUnit.setValue(1)
-        const formulatedUnit = new FormulatedUnit('fahrenheit','F',dimension)
+        const formulatedUnit = new FormulatedUnit('fahrenheit', 'F', basicUnit)
         formulatedUnit.setFormulaFromBase('((a*9/5)+32)')
-        const actualFoUnit = formulatedUnit.convertFromBase(basicUnit)
-        expect(actualFoUnit.name).to.be.equal('fahrenheit')
-        expect(actualFoUnit.value).to.be.equal(33.8)
-        expect(actualFoUnit.symbol).to.be.equal('F')
-        expect(actualFoUnit.dimension.name).to.be.equal('temperature')
-        expect(actualFoUnit.formulatedFromBase).to.be.equal('((a*9/5)+32)')
+        formulatedUnit.convertFromBase()
+        expect(formulatedUnit.value).to.be.equal(33.8)
         done()
     })
 })
